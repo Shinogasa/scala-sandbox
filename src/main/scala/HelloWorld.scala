bbb3f0f0e9de0da32411e035cbce8254c8850ebb
Shinonome <cuulbulu@gmail.com>
2023-08-10 10:40:23 +0900

    Update: for文の練習

--- src/main/scala/HelloWorld.scala
+++ src/main/scala/HelloWorld.scala
@@ -1,5 +1,7 @@
 object HelloWorld {
   def main(args: Array[String]): Unit = {
-    println("Hello, World!")
+    for(x <- 1 to 5; y <- 1 until 5){
+      println("x = " + x + " y = " + y)
+    }
   }
 }
