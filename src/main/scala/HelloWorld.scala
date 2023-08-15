c9af593a350b087acda3a702bf85f7bc9217515b
Shinonome <cuulbulu@gmail.com>
2023-08-15 18:55:01 +0900

    Update: パターンマッチ練習問題

--- src/main/scala/HelloWorld.scala
+++ src/main/scala/HelloWorld.scala
@@ -1,15 +1,23 @@
 object HelloWorld {
   def main(args: Array[String]): Unit = {
-    import java.util.Locale
+    var i: Int = 0
+    while (i <= 1000) {
+      val randList = new scala.util.Random(new java.security.SecureRandom()).alphanumeric.take(5).toList
 
-    val obj: AnyRef = "String Literal"
-    // obj: AnyRef = "String Literal"
-
-    obj match {
-      case v:java.lang.Integer =>
-        println("Integer!")
-      case v:String =>
-        println(v.toUpperCase(Locale.ENGLISH))
+      randList match {
+        case a :: b :: c :: d :: e :: Nil if a == e =>
+          println(randList.mkString)
+          i = i + 1
+        case _ =>
+      }
     }
+//    解答例
+//    for (i <- 1 to 1000) {
+//      val s = new scala.util.Random(new java.security.SecureRandom()).alphanumeric.take(5).toList match {
+//        case List(a, b, c, d, _) => List(a, b, c, d, a).mkString
+//      }
+//      println(s)
+//    }
+
   }
 }
